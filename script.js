7e5e3812237125290d55c6a41ea56ece61f88ae0
avermx <aniket125verma@gmail.com>
2025-03-17 00:30:31 +0530

    init

--- script.js
+++ script.js
@@ -228,7 +228,24 @@
  
 
 
-function sumofdigit(){
+function sumofdigit(num){
+    let sum = 0;
     
+    while(num>0){
+        if(num%2==0){
+            sum  = sum + num%10
+            num = Math.floor(num/10)
+        }
+        else{
+            num = Math.floor(num/10)
+        }
+      
+      
+        
+    }
+    
+    return sum;
 }
 
+console.log(sumofdigit(2384));
+  
